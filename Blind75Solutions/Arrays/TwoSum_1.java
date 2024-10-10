d65a3c16e8faf51e10ae8d67ae4c39439e7bcd35
ashmeetchhabra <ashmeetkchhabra@gmail.com>
2024-10-10 06:43:25 -0700

    Leetcode TwoSum_1 Implementation

--- Blind75Solutions/Arrays/TwoSum_1.java
+++ Blind75Solutions/Arrays/TwoSum_1.java
@@ -1,7 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+import java.util.Map;
 public class TwoSum_1 {
+    static public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> pairInx = new HashMap<>();
+        for(int i = 0; i<nums.length;i++){
+            if(pairInx.containsKey(target-nums[i])){
+                return new int[]{i, pairInx.get(target-nums[i])};
+            }
+            pairInx.put(nums[i], i);
+        }
+        return new int[]{};
+    }
     public static void main(String[] args) {
         System.out.print("Hello and welcome for Two Sum!");
+        int target = 9;
+        int[] res = twoSum(new int[]{2,7,11,15},target);
+        System.out.print("The two sum is: ");
+        for(int i : res){
+            System.out.print(i+" ");
+        }
     }
 }
